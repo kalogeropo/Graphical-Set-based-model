9b6e004ea68c1021628ca1c4fa95c1211e4007eb
Nikitas Rigas Kalogeropoulos <kalogeropo@ceid.upatras.gr>
2021-07-10 13:58:39 +0300

    new main for more tests and more optimized and optimized main automation
    Signed-off-by: Nikitas Rigas Kalogeropoulos <kalogeropo@ceid.upatras.gr>

--- mainautomation.py
+++ mainautomation.py
@@ -7,31 +7,64 @@ def PrepareForNextRound(counter,test_str):
     path = "figures"
     namelist = [str(counter)," ", test_str]
     name = "".join(namelist)
+    print(name)
     dirpath = os.path.join(path, name)
-    os.rename("figures/allq",dirpath)
-    os.chmod(dirpath, 0o444)
-    os.mkdir("figures/allq")
-    shutil.copy2("C:/Users/nrk_pavilion/PycharmProjects/Graphical-Set-based-model/new_res.xlsx", dirpath)
-    targets = ["DotSplit.dat","NegMain.dat","invertedindex.dat","PerSplit.dat","docinfo.dat","ConstantWindFile.dat","SenParConWind.dat"]
+    print(dirpath)
+    try:
+        os.mkdir("figures/allq")
+    except:
+        print("EXISTS")
+    try:
+        os.mkdir(dirpath)
+    except:
+        print("EXISTS")
+    #os.rename("figures/allq",dirpath)
+    #os.chmod(dirpath, 0o444)
+    #os.mkdir("figures/allq")
+    targets = ["DotSplit.dat","NegMain.dat","invertedindex.dat","PerSplit.dat",
+               "docinfo.dat","ConstantWindFile.dat","SenParConWind.dat",
+                "densfile.dat","newfile.dat","debuglog.dat","invertedindex.dat","CoreRankfile.dat","new_res.xlsx"]
     for file in os.listdir("C:/Users/nrk_pavilion/PycharmProjects/Graphical-Set-based-model"):
         if file in targets:
-            shutil.copy2(file, dirpath)
+            try:
+                shutil.copy2(file, dirpath)
+            except FileNotFoundError:
+                print(file + "NOT EXists")
+            try:
+                os.remove(file)
+            except FileNotFoundError:
+                print(file + "NOT EXists to delete")
+
 
 counter = 0
 
-os.system('python main.py 700 0.2 1 1 3 200 0.091')
+"""os.system('python main.py 700 0.2 1 1 3 200 0.091')
 os.system('python main.py 700 0.2 1 2 3 200 0.091')
 os.system('python main.py 700 0.2 1 3 0 200 0.091')
 os.system('python main.py 700 0.2 1 4 3 200 0.091')
 os.system('python main.py 700 0.2 2')
 
-PrepareForNextRound(counter,"700 0.2 3 200 0.091 noprune")
+testname = "700 0.2 3 200 0.091 noprune"
+
+PrepareForNextRound(counter,"testname")
 counter+=1
-#os.remove("DotSplit.dat")
-#os.remove("NegMain.dat")
-#os.remove("invertedindex.dat")
-#os.remove("PerSplit.dat")
-#os.remove("ConstantWindFile.dat")
-#os.remove("SenParConWind.dat")
-#os.remove("docinfo.dat")
+
+"""
+#name of main: sys.args[1] =  h - sys.args[2] =  p - sys.args[3] = menu 1 - sys.args[4] = choice of menu 1 -
+#sys.args[5] = constant window size, sys.args[6] = paragraph size - sys.args[7] = percentage window.
+#main_v2.py 700 0.2 1 1 3 200 0.091')
+
+#os.system('python main_v2.py 700 0.2 1 1 3 200 0.091')
+#os.system('python main_v2.py 700 0.2 1 2 3 200 0.091')
+#os.system('python main_v2.py 700 0.2 1 3 0 200 0.091')
+#os.system('python main_v2.py 700 0.2 1 4 3 200 0.091')
+#os.system('python main_v2.py 700 0.2 1 5 3 200 0.091')
+
+#os.system('python main_v2.py 700 0.2 2')
+
+testname = "testname"
+
+
+PrepareForNextRound(counter,testname)
+counter+=1
 
